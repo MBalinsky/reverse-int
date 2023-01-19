6eaa5afe731e71f9766436705fc004a16bab7843
MBalinsky <supermike820@gmail.com>
2023-01-19 14:33:07 +0300

    feat: some coding

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+      // return +Math.abs(n).toString().split('').reverse().join('');
+      let s = '';
+      let str = Math.abs(n).toString();
+       for(let i = str.length -1; i >= 0; i--){
+           s = s + str[i];
+       }
+         return +s;
 }
+
